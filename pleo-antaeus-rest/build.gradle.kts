c5602a169919788b83b08d123bdeec7a9b3d598b
Cristian Ebbens <cebbens@gmail.com>
2019-08-29 17:26:32 -0300

    Updated javalin and Jackson-databind dependencies.

--- pleo-antaeus-rest/build.gradle.kts
+++ pleo-antaeus-rest/build.gradle.kts
@@ -8,6 +8,6 @@ dependencies {
     implementation(project(":pleo-antaeus-core"))
     implementation(project(":pleo-antaeus-models"))
 
-    implementation("io.javalin:javalin:2.6.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.8")
+    implementation("io.javalin:javalin:3.4.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9.3")
 }
